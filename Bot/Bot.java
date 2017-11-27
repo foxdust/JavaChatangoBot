0088d340005f3b1b9c16439ef80615d9a6076499
foxdust <drakune@gmail.com>
2017-11-27 00:25:14 -0600

    Fixed typos

--- Bot/Bot.java
+++ Bot/Bot.java
@@ -1,4 +1,4 @@
-package bot;
+package Bot;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
--- Bot/Room.java
+++ Bot/Room.java
@@ -1,4 +1,4 @@
-package bot;
+package Bot;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
